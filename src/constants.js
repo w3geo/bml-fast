21130e8e03be59e15d00aa96016c510b31e8b86b
Andreas Hocevar <andreas.hocevar@gmail.com>
2024-06-22 22:47:25 +0200

    Changes for 2024-1 schlaege data

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const SCHLAEGE_SOURCE = 'invekos_schlaege_2023_polygon';
+export const SCHLAEGE_SOURCE = 'invekos_schlaege_20241_polygon';
 export const INITIAL_EXTENT = [8.782379, 46.35877, 17.5, 49.037872];
