3fb39173300b68b9115470af6843ede20af4eb4a
Andreas Hocevar <andreas.hocevar@gmail.com>
2025-02-17 10:58:03 +0100

    Use Agraratlas production data

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,4 @@
 export const SCHLAEGE_SOURCE = 'invekos_schlaege_polygon';
 export const SCHLAEGE_LAYER = 'invekos_schlaege_polygon-fill';
-export const AGRARATLAS_STYLE_URL =
-  'https://assets.w3geo.at/5b85f2c6-1646-4cae-b2e7-94e586c5305b/map/style.json';
+export const AGRARATLAS_STYLE_URL = 'https://agraratlas.inspire.gv.at/map/style.json';
 export const INITIAL_EXTENT = [8.782379, 46.35877, 17.5, 49.037872];
