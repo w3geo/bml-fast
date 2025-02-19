b8e6d38870149fbddd14bbff25650f4ce76f9824
Andreas Hocevar <andreas.hocevar@gmail.com>
2025-02-19 20:54:03 +0100

    Use new dedicated pmtiles style url for agraratlas

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const SCHLAEGE_SOURCE = 'invekos_schlaege_polygon';
 export const SCHLAEGE_LAYER = 'invekos_schlaege_polygon-fill';
-export const AGRARATLAS_STYLE_URL = 'https://agraratlas.inspire.gv.at/map/style.json';
+export const AGRARATLAS_STYLE_URL = 'https://agraratlas.inspire.gv.at/map/style-pmtiles.json';
 export const INITIAL_EXTENT = [8.782379, 46.35877, 17.5, 49.037872];
